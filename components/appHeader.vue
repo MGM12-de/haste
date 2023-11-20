87d934d4d2eb92689f685a4c56c0ea302c461a57
Miguel <miggi92@users.noreply.github.com>
2023-11-20 21:26:56 +0000

    Add appHeader component and update dependencies

---
+++ components/appHeader.vue
@@ -0,0 +1,27 @@
+<template>
+    <UHeader :links="links">
+        <template #logo>
+            <Logo class="w-auto h-6" />
+        </template>
+
+        <template #right>
+            <UColorModeButton />
+        </template>
+
+        <template #panel>
+            <UAsideLinks :links="links" />
+        </template>
+    </UHeader>
+</template>
+
+<script setup lang="ts">
+const links = [{
+    label: 'Documentation',
+    icon: 'i-heroicons-book-open',
+    to: '/getting-started'
+}, {
+    label: 'Pro',
+    icon: 'i-heroicons-square-3-stack-3d',
+    to: '/pro'
+}]
+</script>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,3 +1,5 @@
 <template>
-    
+    <div>
+        <appHeader />
+    </div>
 </template>
