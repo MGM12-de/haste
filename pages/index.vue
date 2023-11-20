3ddb567bab62b6d9062d6b459f1eabed61745bc5
Miguel <miggi92@users.noreply.github.com>
2023-11-20 21:47:14 +0000

    Add about, index, and login pages

--- pages/index.vue
+++ pages/index.vue
@@ -1,2 +1,8 @@
 <template>
+    <ULandingHero title="Handball statistic expert"
+        description="With this site, you can enhance and fully track your handball game statistics."
+        :links="[{ label: 'Get Started', icon: 'i-heroicons-rocket-launch', size: 'lg', to: '/login' }, { label: 'Learn more', trailingIcon: 'i-heroicons-arrow-small-right', color: 'gray', size: 'lg', to: '/about' }]" />
 </template>
+<script setup lang="ts">
+
+</script>
