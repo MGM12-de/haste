f17e0759411eb08bad9aafab80f4867677c5bb7d
Miguel <miggi92@users.noreply.github.com>
2023-11-21 01:12:28 +0100

    Aktualisieren von appHeader.vue

--- components/appHeader.vue
+++ components/appHeader.vue
@@ -7,7 +7,7 @@
         <template #right>
             <UColorModeButton />
             <UPopover v-if="user">
-                <UAvatar src="https://avatars.githubusercontent.com/u/739984?v=4" alt="Avatar" />
+                <UAvatar alt="custom user" />
                 <template #panel>
                     <UVerticalNavigation :links="userLinks" />
                 </template>
