f7e1b640677f2c122047869243f20f9034c087e6
Miguel <miggi92@users.noreply.github.com>
2023-11-30 23:19:49 +0000

    Refactor layout structure and add UContainer component

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,13 +1,18 @@
 <template>
-    <div class="w-screen h-screen">
-        <div>
+    <div class="flex flex-col min-h-screen">
+        <div class="sticky w-full top-0 z-50">
             <appHeader />
         </div>
         <UNotifications />
-        <div class="w-auto h-auto ml-10 mr-10">
-            <slot />
+        <div class="flex-grow ml-10 mr-10">
+            <UContainer>
+
+                <slot />
+            </UContainer>
         </div>
-        <div class="fixed bottom-0 w-full">
+
+        <div
+            class="sticky bottom-0 w-full z-50 bg-background/75 backdrop-blur border-b border-gray-200 dark:border-gray-800 -mb-px">
             <appFooter />
         </div>
     </div>
