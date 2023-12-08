6450c939fcd2ab85970a4447e3cf04072930b99a
Miguel <miggi92@users.noreply.github.com>
2023-12-08 15:49:44 +0000

    Add teams table and types for Team

--- pages/teams.vue
+++ pages/teams.vue
@@ -3,6 +3,14 @@
         <UPageHeader title="Teams" />
         <UPageBody>
 
+            <UTable :rows="teams" />
+
         </UPageBody>
     </UPage>
 </template>
+
+<script setup lang="ts">
+import type { Team } from '~/types';
+
+var teams: Team[] = [];
+</script>
