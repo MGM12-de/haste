5bbf12a188443fac106801a9a68460f5c3ff9c28
Miguel <miggi92@users.noreply.github.com>
2023-12-11 11:52:12 +0000

    updated games paths

---
+++ pages/games/[id].vue
@@ -0,0 +1,19 @@
+<template>
+    <UPage>
+        <UPageHeader :title="game.title" />
+        <UPageBody>
+        </UPageBody>
+    </UPage>
+</template>
+
+<script setup lang="ts">
+const route = useRoute()
+const supabase = useSupabaseClient()
+
+const { data: game, error } = await useAsyncData(`game`, async () => {
+    const { data } = await supabase.from('games').select('*').eq('id', route.params?.id).single();
+    return data
+});
+
+
+</script>
