731269bb6ebd9c4797919d7710d729b792bd1f8c
Miguel Gebhardt <miguel@mgm12.de>
2023-12-13 00:33:36 +0100

    added stripe

--- config/index.ts
+++ config/index.ts
@@ -3,3 +3,4 @@
 export * from './pwa'
 export * from './supabase'
 export * from './content'
+export * from './stripe'
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,19 +1,20 @@
-import { pwa, supabase, content } from "./config";
+import { pwa, supabase, content, stripe } from "./config";
 
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
   extends: ["@nuxt/ui-pro"],
-  modules: ['@nuxt/content', '@nuxt/ui', '@nuxtjs/supabase', '@nuxt/image', "@vite-pwa/nuxt",'@nuxtseo/module', 'nuxt-schema-org'],
+  modules: ['@nuxt/content', '@nuxt/ui', '@nuxtjs/supabase', '@nuxt/image', "@vite-pwa/nuxt", '@nuxtseo/module', 'nuxt-schema-org', '@unlok-co/nuxt-stripe'],
   site: {
     url: 'https://haste.mgm12.dev',
     name: 'HaStE',
   },
   ui: {
     icons: ["mdi"]
-},
+  },
   pwa,
   supabase,
   content,
+  stripe,
 })
