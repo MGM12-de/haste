853e0499e5b72c9704120a7100b4e750c2d7e1c3
Miguel Gebhardt <miguel@mgm12.de>
2023-12-12 23:25:12 +0100

    updated seo

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,7 +5,7 @@ import { pwa, supabase, content } from "./config";
 export default defineNuxtConfig({
   devtools: { enabled: true },
   extends: ["@nuxt/ui-pro"],
-  modules: ['@nuxt/content', '@nuxt/ui', '@nuxtjs/supabase', '@nuxt/image', "@vite-pwa/nuxt",'nuxt-schema-org'],
+  modules: ['@nuxt/content', '@nuxt/ui', '@nuxtjs/supabase', '@nuxt/image', "@vite-pwa/nuxt",'@nuxtseo/module', 'nuxt-schema-org'],
   site: {
     url: 'https://haste.mgm12.dev',
     name: 'HaStE',
