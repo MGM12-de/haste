05b5d3b051502279b5c3916884d7cc242e026195
Miguel Gebhardt <miguel@mgm12.de>
2023-12-13 01:25:17 +0100

    updated pwa config

--- config/pwa.ts
+++ config/pwa.ts
@@ -1,55 +1,60 @@
 import type { ModuleOptions } from "@vite-pwa/nuxt";
 
 export const pwa: ModuleOptions = {
-    registerType: "autoUpdate",
-    manifest: {
-      categories: ["handball", "statistics", "sport"],
-      name: "Handball statistic expert",
-      short_name: "HaStE",
-      description: "Handball statistics App",
-      theme_color: "#0ea5e9",
-      lang: "en",
-      icons: [
-        {
-          src: "pwa-64x64.png",
-          sizes: "64x64",
-          type: "image/png",
-        },
-        {
-          src: "pwa-192x192.png",
-          sizes: "192x192",
-          type: "image/png",
-        },
-        {
-          src: "pwa-512x512.png",
-          sizes: "512x512",
-          type: "image/png",
-        },
-        {
-          src: "maskable-icon-512x512.png",
-          sizes: "512x512",
-          type: "image/png",
-          purpose: "any maskable",
-        },
-        {
-          src: "apple-touch-icon-180x180.png",
-          sizes: "180x180",
-          type: "image/png",
-          purpose: "any maskable",
-        },
-      ],
-    },
-    workbox: {
-      navigateFallback: "/",
-      globPatterns: ["**/*.{js,css,html,png,svg,ico}"],
-    },
-    client: {
-      installPrompt: true,
-    },
-    devOptions: {
-      enabled: true,
-      suppressWarnings: true,
-      navigateFallbackAllowlist: [/^\/$/],
-      type: "module",
-    },
-  };
+  registerType: "autoUpdate",
+  manifest: {
+    categories: ["handball", "statistics", "sport"],
+    name: "Handball statistic expert",
+    short_name: "HaStE",
+    description: "Handball statistics App",
+    theme_color: "#0ea5e9",
+    background_color: "#808080",
+    scope: "/",
+    start_url: "/",
+    display: "standalone",
+    lang: "en",
+    icons: [
+      {
+        src: "pwa-64x64.png",
+        sizes: "64x64",
+        type: "image/png",
+      },
+      {
+        src: "pwa-192x192.png",
+        sizes: "192x192",
+        type: "image/png",
+      },
+      {
+        src: "pwa-512x512.png",
+        sizes: "512x512",
+        type: "image/png",
+      },
+      {
+        src: "maskable-icon-512x512.png",
+        sizes: "512x512",
+        type: "image/png",
+        purpose: "any maskable",
+      },
+      {
+        src: "apple-touch-icon-180x180.png",
+        sizes: "180x180",
+        type: "image/png",
+        purpose: "any maskable",
+      },
+    ],
+  },
+  includeAssets: ['**/*'],
+  workbox: {
+    navigateFallback: "/",
+    globPatterns: ["**/*.{js,css,html,png,svg,ico}"],
+  },
+  client: {
+    installPrompt: true,
+  },
+  devOptions: {
+    enabled: true,
+    suppressWarnings: true,
+    navigateFallbackAllowlist: [/^\/$/],
+    type: "module",
+  },
+};
