f2e328fa3e5757be227a70edeaec649be3df8621
Miguel <miggi92@users.noreply.github.com>
2024-01-05 11:44:52 +0000

    removed redirection

--- config/supabase.ts
+++ config/supabase.ts
@@ -3,9 +3,9 @@ import type { ModuleOptions } from "@nuxtjs/supabase";
 
 export const supabase: ModuleOptions = {
   // redirect: true,
-  redirectOptions: {
-    login: '/login',
-    callback: '/confirm',
-    exclude: ['/', '/prices', '/about', '/imprint', '/contact'],
-  }
+  // redirectOptions: {
+  //   login: '/login',
+  //   callback: '/confirm',
+  //   exclude: ['/', '/prices', '/about', '/imprint', '/contact'],
+  // }
 }
