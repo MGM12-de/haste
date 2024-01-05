584b8a2f27d46af5c8e8b4b58b133a46b331c7b5
Miguel <miggi92@users.noreply.github.com>
2024-01-05 11:49:50 +0000

    revert changes

--- config/supabase.ts
+++ config/supabase.ts
@@ -2,10 +2,10 @@ import type { ModuleOptions } from "@nuxtjs/supabase";
 
 
 export const supabase: ModuleOptions = {
-  // redirect: true,
-  // redirectOptions: {
-  //   login: '/login',
-  //   callback: '/confirm',
-  //   exclude: ['/', '/prices', '/about', '/imprint', '/contact'],
-  // }
+  redirect: true,
+  redirectOptions: {
+    login: '/login',
+    callback: '/confirm',
+    exclude: ['/', '/prices', '/about', '/imprint', '/contact'],
+  }
 }
