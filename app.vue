eaed9a4763a33be5f463285a81a7a6cddfd24836
Miguel <miggi92@users.noreply.github.com>
2024-02-24 10:44:54 +0000

    feat: :sparkles: added pwa refresh toast

--- app.vue
+++ app.vue
@@ -1,3 +1,23 @@
+<script setup lang="ts">
+const { $pwa } = useNuxtApp()
+const toast = useToast()
+
+onMounted(() => {
+  if ($pwa.offlineReady)
+    toast.add({ title: 'Offline rdy', description: 'App ready to work offline' })
+  if ($pwa.needsRefresh) {
+    toast.add({
+      title: 'New version',
+      description: 'New version available, please refresh',
+      actions: [{
+        label: 'Refresh',
+        click: () => $pwa.updateServiceWorker(),
+      }],
+    })
+  }
+})
+</script>
+
 <template>
   <NuxtLayout>
     <NuxtLoadingIndicator />
@@ -5,4 +25,3 @@
     <NuxtPage />
   </NuxtLayout>
 </template>
-
